e0d2270b78206249fb92bc58bb55e3b2c3e5af2a
yohannngami2711-beep <yohannngami2711@gmail.com>
2025-10-21 10:28:31 +0200

    Add files via upload

---
+++ TP calculatrice/calculatrice.js
@@ -0,0 +1,26 @@
+function somme() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) + Number(b);
+    document.getElementById("resultat").value = c;
+}
+function multiplier() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) * Number(b);
+    document.getElementById("resultat").value = c;
+}
+function diviser() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) / Number(b);
+    document.getElementById("resultat").value = c;
+
+}
+function soustraire() {
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a) - Number(b);
+    document.getElementById("resultat").value = c;
+
+}
